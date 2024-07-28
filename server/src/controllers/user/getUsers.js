97e8ab0f8f24b630583f20a77dbf16a57a4ddd84
liz-garcia <lizgarciamarin@gmail.com>
2024-07-28 10:00:22 +0200

    Reset database connection.

--- server/src/controllers/user/getUsers.js
+++ server/src/controllers/user/getUsers.js
@@ -1,8 +1,12 @@
-// import User from "../../models/User.js";
+import User from "../../models/User.js";
 
 export const getUsers = async (req, res) => {
-  const message = { message: "Now hello from users!!" };
+  const users = await User.find();
+  const message = { message: `${users}` };
+
+  // const message = { message: "Now hello from users!!" };
   res.json(message);
+
   // try {
   //   const users = await User.find();
   //   res.status(200).json({ success: true, result: users });
