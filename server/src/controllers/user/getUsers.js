74a7468f9dec9a0a9687493d5d188c7aba4144e9
liz-garcia <lizgarciamarin@gmail.com>
2024-07-28 18:47:54 +0200

    Updated getUsers.

--- server/src/controllers/user/getUsers.js
+++ server/src/controllers/user/getUsers.js
@@ -1,19 +1,13 @@
 import User from "../../models/User.js";
 
 export const getUsers = async (req, res) => {
-  const users = await User.find();
-  const message = { message: `${users}` };
-
-  // const message = { message: "Now hello from users!!" };
-  res.json(message);
-
-  // try {
-  //   const users = await User.find();
-  //   res.status(200).json({ success: true, result: users });
-  // } catch (error) {
-  //   console.error(error);
-  //   res
-  //     .status(500)
-  //     .json({ success: false, msg: "Unable to get users, try again later." });
-  // }
+  try {
+    const users = await User.find();
+    res.status(200).json({ success: true, result: users });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ success: false, msg: "Unable to get users, try again later" });
+  }
 };
