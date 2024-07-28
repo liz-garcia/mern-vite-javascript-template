8669140feeb28cba6cbf5214452e2ef02be0b1b2
liz-garcia <lizgarciamarin@gmail.com>
2024-07-28 18:43:01 +0200

    Improved error handling at server/connectDB.

--- server/src/db/connectDB.js
+++ server/src/db/connectDB.js
@@ -1,5 +1,17 @@
 import mongoose from "mongoose";
 
-const connectDB = () => mongoose.connect(process.env.MONGODB_URL);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("\x1b[32m%s\x1b[0m", "MongoDB connected successfully");
+  } catch (error) {
+    console.error(
+      "\x1b[31m%s\x1b[0m",
+      "Failed to connect to MongoDB",
+      error.message
+    );
+    process.exit(1);
+  }
+};
 
 export default connectDB;
