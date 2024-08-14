4150d432355d8900525edb8a3fc231d7416b701e
liz-garcia <lizgarciamarin@gmail.com>
2024-08-14 15:04:28 +0200

    Removed comments on vite.config.

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,14 +8,4 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Default is "" because on our heroku servers
-  // we want to have it default to our current URL
-  // Test if we really need the define property:
-  // define: {
-  //   "import.meta.env.VITE_BASE_SERVER_URL": JSON.stringify(
-  //     process.env.NODE_ENV === "production"
-  //       ? process.env.UI_BASE_URL || ""
-  //       : process.env.VITE_BASE_SERVER_URL || ""
-  //   ),
-  // },
 });
